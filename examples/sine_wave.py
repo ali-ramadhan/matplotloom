bca33359108a4a564695fb1296be15e6e6f4d685
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2024-05-08 16:59:28 -0600

    Add sine wave example

---
+++ examples/sine_wave.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotloom import Loom
+
+with Loom("sine_wave_animation.gif", fps=30) as loom:
+    for phase in np.linspace(0, 2*np.pi, 100):
+        fig, ax = plt.subplots()
+
+        x = np.linspace(0, 2*np.pi, 200)
+        y = np.sin(x + phase)
+        
+        ax.plot(x, y)
+        ax.set_xlim(0, 2*np.pi)
+        
+        loom.save_frame(fig)
