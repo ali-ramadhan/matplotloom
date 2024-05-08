cf225d94d0076cf5f46b4ecb092f7e9f86619369
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2024-05-08 17:32:05 -0600

    Add animations from examples to show in README

--- examples/sine_wave.py
+++ examples/sine_wave.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotloom import Loom
 
-with Loom("sine_wave_animation.gif", fps=30) as loom:
+with Loom("sine_wave.gif", fps=30) as loom:
     for phase in np.linspace(0, 2*np.pi, 100):
         fig, ax = plt.subplots()
 
