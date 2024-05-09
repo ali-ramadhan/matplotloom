1d38374d1335ee8fb599422656a4bdaf01e6d065
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2024-05-08 20:40:39 -0600

    Fix paths in `test_examples.py`

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -2,10 +2,15 @@ import pytest
 
 from pathlib import Path
 
+def run_example(name):
+    filepath = Path(__file__).resolve().parent / ".." / "examples" / f"{name}.py"
+    exec(filepath.read_text())
+    return
+
 def test_sine_wave():
-    exec(Path("../examples/sine_wave.py").read_text())
+    run_example("sine_wave")
     assert Path("sine_wave.gif").is_file()
 
 def test_rotating_circular_sine_wave():
-    exec(Path("../examples/rotating_circular_sine_wave.py").read_text())
+    run_example("rotating_circular_sine_wave")
     assert Path("rotating_circular_sine_wave.mp4").is_file()
